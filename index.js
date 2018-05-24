1118be9200ee19a5f1fd413244938ed42582efe2
Gabriel Valle <gvalle91@gmail.com>
2018-05-24 22:54:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,3 +20,11 @@ function appendKitten(name){
   var newKittens = [...kittens, name]
   return newKittens
 }
+function prependKitten(name){
+  var newKittens = [name, ...kittens]
+  return newKittens
+}
+function removeLastKitten(){
+  var newKittens = kittens.slice(0,kittens.length - 1)
+  return newKittens
+}
